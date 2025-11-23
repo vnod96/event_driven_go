549126c58b33cae02f72fce66ff0b94a87d975a6
vnod96 <vinod.suthar16@gmail.com>
2025-11-23 05:16:51 +0000

    23 Nov 2025, 10:46

---
+++ 03-message-broker/02-publishing-messages/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	logger := watermill.NewSlogLogger(nil)
+
+	rc := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	pub, err := redisstream.NewPublisher(redisstream.PublisherConfig{
+		Client: rc,
+	}, logger)
+
+	if err != nil {
+		fmt.Errorf("failed to start publisher: %v", err)
+	}
+
+	err = pub.Publish("progress", message.NewMessage(watermill.NewUUID(), []byte("50")))
+	if err != nil {
+		fmt.Errorf("failed to publish msg: %v", err)
+	}
+	err = pub.Publish("progress", message.NewMessage(watermill.NewUUID(), []byte("100")))
+	if err != nil {
+		fmt.Errorf("failed to publish msg: %v", err)
+	}
+}
