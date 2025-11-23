55677108fdc07232e4885a175c4e98600c95598d
vnod96 <vinod.suthar16@gmail.com>
2025-11-23 09:56:20 +0000

    23 Nov 2025, 15:26

--- project/http/handler_tickets.go
+++ project/http/handler_tickets.go
@@ -6,47 +6,42 @@ import (
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/labstack/echo/v4"
+
+	"tickets/entities"
 )
 
 type ticketsConfirmationRequest struct {
 	Tickets []string `json:"tickets"`
 }
 
+type TicketStatusRequest struct {
+	TicketID string `json:"ticket_id"`
+	Status string `json:"status"`
+	Price entities.Money  `json:"price"`
+	CustomerEmail string `json:"customer_email"`
+}
+
+type TicketsStatusRequest struct {
+	Tickets []TicketStatusRequest `json:"tickets"`
+}
+
 func (h Handler) PostTicketsConfirmation(c echo.Context) error {
-	var request ticketsConfirmationRequest
+	var request TicketsStatusRequest
 	err := c.Bind(&request)
 	if err != nil {
 		return err
 	}
 
 	for _, ticket := range request.Tickets {
-		err := h.pub.Publish("issue-receipt", message.NewMessage(watermill.NewUUID(), []byte(ticket)))
+		err := h.pub.Publish("issue-receipt", message.NewMessage(watermill.NewUUID(), []byte(ticket.TicketID)))
 
 		if err != nil {
 			return err
 		}
-		err = h.pub.Publish("append-to-tracker", message.NewMessage(watermill.NewUUID(), []byte(ticket)))
+		err = h.pub.Publish("append-to-tracker", message.NewMessage(watermill.NewUUID(), []byte(ticket.TicketID)))
 		if err != nil {
 			return err
 		}
-
-		// h.w.Send(worker.Message{
-		// 	Task:     worker.TaskIssueReceipt,
-		// 	TicketID: ticket,
-		// },
-		// 	worker.Message{
-		// 		Task:     worker.TaskAppendToTracker,
-		// 		TicketID: ticket,
-		// 	})
-		// err = h.receiptsService.IssueReceipt(context.Background(), ticket)
-		// if err != nil {
-		// 	return err
-		// }
-
-		// err = h.spreadsheetsAPI.AppendRow(context.Background(), "tickets-to-print", []string{ticket})
-		// if err != nil {
-		// 	return err
-		// }
 	}
 
 	return c.NoContent(http.StatusOK)
