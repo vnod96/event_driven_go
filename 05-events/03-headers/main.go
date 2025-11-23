28c74ad0edc8551da038dcc1e8aaed47589f21f3
vnod96 <vinod.suthar16@gmail.com>
2025-11-23 09:18:48 +0000

    23 Nov 2025, 14:48

---
+++ 05-events/03-headers/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+type ProductOutOfStock struct {
+	ProductID string `json:"product_id"`
+}
+
+type ProductBackInStock struct {
+	ProductID string `json:"product_id"`
+	Quantity  int    `json:"quantity"`
+}
+
+type Publisher struct {
+	pub message.Publisher
+}
+
+func NewPublisher(pub message.Publisher) Publisher {
+	return Publisher{
+		pub: pub,
+	}
+}
+
+func (p Publisher) PublishProductOutOfStock(productID string) error {
+	event := ProductOutOfStock{
+		ProductID: productID,
+	}
+
+	payload, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	msg := message.NewMessage(watermill.NewUUID(), payload)
+
+	return p.pub.Publish("product-updates", msg)
+}
+
+func (p Publisher) PublishProductBackInStock(productID string, quantity int) error {
+	event := ProductBackInStock{
+		ProductID: productID,
+		Quantity:  quantity,
+	}
+
+	payload, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	msg := message.NewMessage(watermill.NewUUID(), payload)
+
+	return p.pub.Publish("product-updates", msg)
+}
