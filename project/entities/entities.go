60ef7e304d2fe02d5435ff3cd1e6536ed68c6f8c
vnod96 <vinod.suthar16@gmail.com>
2025-11-25 14:45:51 +0000

    25 Nov 2025, 20:15

--- project/entities/entities.go
+++ project/entities/entities.go
@@ -1,5 +1,11 @@
 package entities
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Money struct {
 	Amount string `json:"amount"`
 	Currency string `json:"currency"`
@@ -14,4 +20,24 @@ type AppendToTrackerPayload struct {
 type IssueReceiptPayload struct {
 	TicketID string `json:"ticket_id"`
 	Price Money `json:"price"`
+}
+
+type TicketBookingConfirmed struct {
+	Header MessageHeader `json:"header"`
+
+	TicketID string `json:"ticket_id"`
+	CustomerEmail string `json:"customer_email"`
+	Price Money `json:"price"`
+}
+
+type MessageHeader struct {
+	ID string `json:"id"`
+	PublishedAt time.Time `json:"published_at"`
+}
+
+func NewMessageHeader() MessageHeader {
+	return  MessageHeader{
+		ID: uuid.NewString(),
+		PublishedAt: time.Now().UTC(),
+	}
 }
